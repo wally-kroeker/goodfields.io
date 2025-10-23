d2e2bf646065509176a0a159a1dc97b106d447f8
Wally Kroeker <wallyk@gmail.com>
2025-10-23 11:41:16 -0500

    fix: correct contact email domain

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
     process.env.NEXT_PUBLIC_BOOKING_URL ??
     "https://calendly.com/wally-goodfields/45min",
   contactEmail:
-    process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? "wally@goodfields.ca",
+    process.env.NEXT_PUBLIC_CONTACT_EMAIL ?? "wally@goodfields.io",
   socialLinks: {
     linkedin: "https://www.linkedin.com/in/wallykroeker/",
     website: "https://wallykroeker.com",
